ac3aab0193388c06da4e9eb15b018be851af47e4
Donut <korawit-1715_@hotmail.com>
2020-10-21 18:48:20 +0700

    test

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -5,10 +5,13 @@ public class TestGit {
 
     public static void main(String[] args) {
         sayHello();
+        Test();
 
     }
     public static void sayHello(){
         System.out.println("Hello World");
     }
-    
+    public static void Test(){
+        System.out.println("Test");
+    }
 }
